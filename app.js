28501647bf4c97e667120acda2c723b07ae03fc0
K0 <K080629@outlook.com>
2024-09-18 21:06:32 -0400

    daw
    daw

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-const sides = document.querySelectorAll(".side");
-const tl = gsap.timeline({ repeat: -1 });
+const sides = document.querySelectorAll('.side');
+const tl = gsap.timeline({repeat: -1});
 
 function animateTiles(i) {
-  tl.to(
-    `.side-${i} .tile`,
-    {
-      duration: 0.25,
-      scale: 0.4,
-      rotate: 180,
-      z: "-50vmin",
-      yoyo: true,
-      repeat: 1,
-      repeatDelay: 0.5,
-      stagger: {
-        amount: 1.5,
-        grid: "auto",
-        from: "edges"
-      }
+  tl.to(`.side-${i} .tile`, {
+    duration: 0.3,
+    scale: 0.25,
+    rotate: 180,
+    z: '-50vmin',
+    yoyo: true,
+    repeat: 1,
+    repeatDelay: 0.5,
+    stagger: {
+      amount: 0.6, 
+      grid: 'auto',
+      from: 'edges',
     },
-    i * 0.1
-  );
+  }, i * 0.2);
 }
 
 [...sides].forEach((face, i) => animateTiles(i));
-
-
-
-
-
